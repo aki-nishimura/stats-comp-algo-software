d10516ef6c9b317319b44dde37793410d6063f66
Aki Nishimura <aki.nishimura@jhu.edu>
2024-12-25 18:16:16 -0500

    Allow specifying current year as optional arg in case calendar year hasn't changed

--- extra/update_academic_year.sh
+++ extra/update_academic_year.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-curr_year=$(date +%Y)
+if [ -z "$1" ]; then
+	curr_year=$(date +%Y)
+else
+	curr_year="$1"
+fi
 prev_year=$(($curr_year - 1))
 find . -type f \( -name "*.Rmd" -o -name "*.sh" \) -exec \
 	gsed -i "s/stats-comp-algo-software-${prev_year}/stats-comp-algo-software-${curr_year}/g" {} \;
